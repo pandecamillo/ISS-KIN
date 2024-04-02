3e7529c78b2ba3a3e64828bc62c73199cd7fb10f
pandecamillo <160549468+pandecamillo@users.noreply.github.com>
2024-04-02 18:38:18 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+Hey, i am Camillo. I am the one that created that website. 
+If you want to be my friend, please contact me 
+Whatsapp : +243893196755 
+Facebook : Pande Camillo
---
+++ menu.js
@@ -0,0 +1,16 @@
+const btnMenu = document.getElementById('menu');
+const navBar = document.getElementById('navigation')
+
+let active = false;
+
+btnMenu.addEventListener('click',function(){
+    if(active){
+        navBar.style.display = "none";
+        btnMenu.src = "menu.png";
+        active = false;
+    }else{
+        navBar.style.display = "inline";
+        btnMenu.src = "closemenu.png";
+        active = true;
+    }
+})
